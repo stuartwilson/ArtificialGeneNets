bed2c557423647239e056d714e018d281ec01efe
Stuart Wilson <s.p.wilson@sheffield.ac.uk>
2020-02-07 05:56:02 +0000

    run.py now obtains results on the fly

--- mapIndMat/run.py
+++ mapIndMat/run.py
@@ -2,56 +2,93 @@ import h5py
 import numpy as np
 from conns import *
 import subprocess
-import time
+import sys
 
-h5f = h5py.File('network.h5','w')
 
-N = 32
-n = np.array([N],dtype=int)
+# COMMON
+dstRoot = 'data/expt'
+
 inputs = np.array([0,1,2,3],dtype=int)
 outputs = np.array([4,5,6,7],dtype=int)
 knockouts = np.array([8,9,10,11],dtype=int)
-pre = np.array([],dtype=int)
-post = np.array([],dtype=int)
 
-######## NETWORK SPEC
-pre, post = recur(pre,post,np.arange(N))
-######### NETWORK SPEC
+if(len(sys.argv)<5):
+    print("Specify: Nnodes Nbatch Nsims timesteps")
 
-h5f.create_dataset('N', data=n)
-h5f.create_dataset('inputs', data=inputs)
-h5f.create_dataset('outputs', data=outputs)
-h5f.create_dataset('knockouts', data=knockouts)
-h5f.create_dataset('pre', data=pre)
-h5f.create_dataset('post', data=post)
+Nnodes = int(sys.argv[1])
+Nbatch = int(sys.argv[2])
+Nsims = int(sys.argv[3])
+t = int(sys.argv[4])
 
-h5f.close()
+Seeds = np.arange(Nsims)
+Sizes = np.ones(Nsims)*Nnodes
 
+cullMax = 0.75
+toCull = np.zeros(Nsims,dtype=int)
+for i in range(Nsims):
+    toCull[i] = int(cullMax*(i/(Nsims-1))*Nnodes*Nnodes)
 
 
 ######## RUN THE MODEL
-p = []
-
-t = 100000
-n = 5
-nbatch = 3
-
+j = 0
 k = 0
-while(k<n):
-    for i in range(nbatch):
-        dst = 'data/expt'+str(k)
-        subprocess.run('mkdir '+dst,shell=True)
-        subprocess.run('cp inputs.h5 '+dst+'/inputs.h5',shell=True)
-        subprocess.run('cp network.h5 '+dst+'/network.h5',shell=True)
-        p = np.hstack([p,subprocess.Popen('./../build/sim/greig config.json  '+dst+'  '+str(t)+' '+str(k),shell=True)])
-        k = k+1
+running = True
+while(running):
 
-    ready=False
-    while(not ready):
-        ready=True
-        for i in range(len(p)):
-            if(p[i].poll()is None):
-                ready=False
+    P = []
+    for i in range(Nbatch):
+
+        if not running: break
+
+        dst = dstRoot+str(j)
+        subprocess.run('mkdir '+dst,shell=True)
+
+        ### NETWORK SPEC
+        N = Sizes[j]
+        Narr = np.array([N],dtype=int)
+        pre = np.array([],dtype=int)
+        post = np.array([],dtype=int)
+        pre, post = recur(pre,post,np.arange(N))
+        for c in range(toCull[j]):
+            pre, post = cullRand(pre,post)
+
+
+        ###
+        h5f = h5py.File(dst+'/network.h5','w')
+        h5f.create_dataset('N', data=Narr)
+        h5f.create_dataset('inputs', data=inputs)
+        h5f.create_dataset('outputs', data=outputs)
+        h5f.create_dataset('knockouts', data=knockouts)
+        h5f.create_dataset('pre', data=pre)
+        h5f.create_dataset('post', data=post)
+        h5f.close()
+
+        ###
+        subprocess.run('cp inputs.h5 '+dst+'/inputs.h5',shell=True)
+        P = np.hstack([P,subprocess.Popen('./../build/sim/greig config.json  '+dst+'  '+str(t)+' '+str(Seeds[j]),shell=True)])
+
+        ###
+        running=j<(Nsims-1)
+        j+=1
+
+    waitUntilReady(P)
+
+
+    ######## PERFORM ANALYSIS
+
+    for i in range(Nbatch):
+        if not running: break
+
+        dst = dstRoot+str(k)
+        h5f = h5py.File(dst + '/outputs.h5','r')
+        err = h5f['error'][:]
+        h5f.close()
+        print("Error: "+str(err[-1])+" ("+str(k)+")")
+
+        running=k<(Nsims-1)
+        k+=1
+
+
 
 
 
