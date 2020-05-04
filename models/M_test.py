c112ffad61df88e688ea96a9dda7c02ab48cf210
stuartpwilson <s.p.wilson@sheffield.ac.uk>
2020-05-04 13:10:55 +0100

    added encoding info to top

--- models/M_test.py
+++ models/M_test.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import h5py
 import numpy as np
 from tools import *
 import os
 
 # Parameters
-dir = 'test' # This folder should exist! It should contain a config.json file
+dir = 'test' # This folder should exist. It should contain a config.json file
 steps = 10000
 seed = 3
 N = 5
@@ -27,8 +29,8 @@ h5f.create_dataset('post', data=post)
 h5f.close()
 
 # Run the model
-os.system('./../build/src/modelVis '+dir+' '+str(steps)+' '+str(seed))
+os.system('./../build/src/model '+dir+' '+str(steps)+' '+str(seed))
 
-print("run './../build/src/modelVis "+dir+" 0 0' to see the results")
+print("run './../build/src/model "+dir+" 0 0' to see the results")
 
 
